9f0a51846120c1e1fdae8f83f980146b73a35291
sanilUnstudio <sanil@unstudio.ai>
2025-01-22 14:02:56 +0530

    increase time limit

--- src/app/api/upscale/route.tsx
+++ src/app/api/upscale/route.tsx
@@ -4,6 +4,8 @@ import { NextResponse } from "next/server";
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN,
 });
+export const maxDuration = 300; // Set max duration to 300 seconds (5 minutes)
+
 
 export async function POST(request: Request) {
     try {
